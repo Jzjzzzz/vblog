edbb10af54eeae0550c9d8551f8bedbbef2197fc
Jzj <946232976@qq.com>
2022-08-29 19:06:27 +0800

    修改部分配置文件

--- vue-admin/vue.config.js
+++ vue-admin/vue.config.js
@@ -24,10 +24,10 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
-  lintOnSave: process.env.NODE_ENV === 'development', 
+  lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
     port: port,
