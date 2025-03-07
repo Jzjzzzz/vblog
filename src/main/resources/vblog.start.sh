8f701f113b17fdec3f871719255bc65d009968f7
Jzj <946232976@qq.com>
2025-03-07 10:00:37 +0800

    优化镜像打包脚本

--- src/main/resources/vblog.start.sh
+++ src/main/resources/vblog.start.sh
@@ -1,9 +1,10 @@
-SOURCE_PATH=/usr/local/docker
+#!/bin/sh
+
 SERVER_NAME=vblog.jar
 TAG=latest
 SERVER_PORT=8081
 CID=$(docker ps | grep "$SERVER_NAME" | awk '{print $1}')
-IID=$(docker images | grep "$SERVER_NAME:$TAG" | awk '{print $3}')
+IID=$(docker images | grep "^${SERVER_NAME}\s*${TAG}" | awk '{print $3}')
 if [ -n "$CID" ]; then
   echo "存在容器$SERVER_NAME, CID-$CID"
   docker stop $CID
@@ -12,9 +13,9 @@ fi
 if [ -n "$IID" ]; then
   echo "存在$SERVER_NAME:$TAG镜像，IID=$IID"
   docker rmi $IID
+  docker build -t $SERVER_NAME:$TAG .
 else
   echo "不存在$SERVER_NAME:$TAG镜像，开始构建镜像"
-  cd $SOURCE_PATH
   docker build -t $SERVER_NAME:$TAG .
 fi
 docker run --name $SERVER_NAME -v /usr/local/upload:/usr/local/upload -d -p $SERVER_PORT:$SERVER_PORT $SERVER_NAME:$TAG
